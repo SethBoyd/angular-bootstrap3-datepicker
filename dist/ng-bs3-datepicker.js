9ea5ec81dcb316851fd4ab2d4163a1b9e59ce816
SethBoyd <sethdboyd@gmail.com>
2016-05-12 16:33:32 -0600

    fix validation

--- dist/ng-bs3-datepicker.js
+++ dist/ng-bs3-datepicker.js
@@ -4,14 +4,20 @@ dp = angular.module('ng-bs3-datepicker', []);
 
 dp.directive('ngBs3Datepicker', function($compile, $timeout) {
   return {
+    require: "ngModel",
     restrict: 'E',
     replace: true,
-    template: "<div class='input-group date'>\n  <input type='text' class='form-control'/>\n  <span class='input-group-addon'>\n    <span class='fa fa-calendar'></span>\n  </span>\n</div>",
+    template: "<div class='input-group date'>\n  <input type='text' class='form-control' ng-model='inputModel' ng-required='inputRequired' ng-model-options=\"{ updateOn: 'blur' }\"/>\n  <span class='input-group-addon'>\n    <span class='glyphicon glyphicon-calendar'></span>\n  </span>\n</div>",
+
     scope: {
       datePickerOptions: '=',
-      dateFormat: '='
+      dateFormat: '=',
+      dateRequired: '=',
+      bindModel:'=ngModel'
     },
-    link: function($scope, element, attr) {
+    link: function(scope, element, attr, ctrl) {
+      scope.inputModel = "";
+      scope.inputRequired = scope.dateRequired ? true : false;
 
       var attributes, input, resetValue, initialized;
       attributes = element.prop("attributes");
@@ -23,59 +29,50 @@ dp.directive('ngBs3Datepicker', function($compile, $timeout) {
         }
       });
 
-      $scope.reset = function() {
+      scope.reset = function() {
         var datePicker = input.data('DateTimePicker');
         if (datePicker) {
-          datePicker.enabledDates($scope.datePickerOptions.enabledDates);
+          datePicker.enabledDates(scope.datePickerOptions.enabledDates);
         } else {
-          return $timeout($scope.reset, 100);
+          return $timeout(scope.reset, 100);
         }
       };
 
-      $scope.$watchCollection('datePickerOptions.enabledDates', function(value) {
-        return $scope.reset()
+      scope.$watchCollection('datePickerOptions.enabledDates', function(value) {
+        return scope.reset()
       });
       element.find('.input-group-addon').on('click', function(e) {
         return element.find('input').focus();
       });
-      element.on("change.dp", function(e) {
-        return $scope.$apply(function() {
-          var i, obj, objPath, path, _i, _len, _results;
-          if (e.date) {
-            objPath = attr.ngModel.split(".");
-            obj = $scope;
-            _results = [];
-            for (i = _i = 0, _len = objPath.length; _i < _len; i = ++_i) {
-              path = objPath[i];
-              if (!obj[path]) {
-                obj[path] = {};
-              }
-              if (i === objPath.length - 1) {
-                if (resetValue) {
-                  resetValue = false;
-                  _results.push(obj[path] = null);
-                } else {
-                  _results.push(obj[path] = e.date.format($scope.datePickerOptions.dateFormat));
-                }
-              } else {
-                _results.push(obj = obj[path]);
-              }
-            }
-            return _results;
-          }
-        });
-      });
-      $scope.$watch(attr.ngModel, function(newValue, oldValue) {
+      scope.$watch(attr.ngModel, function(newValue, oldValue) {
         if (oldValue && !newValue) {
           return resetValue = true;
         }
       });
 
       $timeout(function () {
-        input.datetimepicker($scope.datePickerOptions);
+        input.datetimepicker(scope.datePickerOptions)
+          .on("dp.change", function(e) {
+            $timeout(function() {
+              var dtp = input.data('DateTimePicker');
+              scope.bindModel = dtp.date();
+              if (scope.dateRequired) {
+                if (ctrl.$isEmpty(scope.bindModel)) {
+                   scope.inputRequired = true;
+                } else {
+                  var enabledDates = dtp.enabledDates();
+                  if (enabledDates) {
+                    scope.inputRequired = !enabledDates[scope.bindModel.format('YYYY-MM-DD')];
+                  } else {
+                   scope.inputRequired = false;
+                  }
+                }
+              }
+            });
+          });
       });
 
-      return $compile(input)($scope);
+      return $compile(input)(scope);
     }
   };
 });
